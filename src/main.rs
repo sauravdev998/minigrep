ad0129fccf777f515d6ab7cb953dcc8d8d41a53c
Saurav Kumar Mohanta <sauravkumarmohanta584@gmail.com>
2026-01-17 05:41:03 +0530

    added stdeer for the errors

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,13 @@ use std::{env, error::Error, fs, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config: Config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
     println!("In file {}", config.file_path);
 
     if let Err(e) = run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
